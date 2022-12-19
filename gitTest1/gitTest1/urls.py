8d8774c07178f002aaffd35dd170e356f5057c17
masu129 <mskth.1228@gmail.com>
2022-12-20 00:06:55 +0900

    title修正test

--- gitTest1/gitTest1/urls.py
+++ gitTest1/gitTest1/urls.py
@@ -1,5 +1,5 @@
 """gitTest1 URL Configuration
-"""test
+
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
